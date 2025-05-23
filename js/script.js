78267a8eb812b7e3a14a69c2e74964e255a2926d
Yuri <yuricastro610@gmail.com>
2025-05-22 22:01:20 -0300

    Interface para download de site completa

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const questions = document.querySelectorAll(".question");
+
+questions.forEach((question) => {
+  question.addEventListener("click", () => {
+    const answer = question.nextElementSibling;
+
+    document.querySelectorAll(".answer").forEach((el) => {
+      if (el !== answer) {
+        el.style.display = "none";
+      }
+    });
+
+    if (answer.style.display === "block") {
+      answer.style.display = "none";
+    } else {
+      answer.style.display = "block";
+    }
+  });
+});
