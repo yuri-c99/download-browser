68931eac5ea2d9f10129a0a8ab2a3c12849c0018
Yuri <yuricastro610@gmail.com>
2025-05-22 22:18:53 -0300

    Interface para instalação de navegador

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 const questions = document.querySelectorAll(".question");
-
 questions.forEach((question) => {
   question.addEventListener("click", () => {
     const answer = question.nextElementSibling;
-
     document.querySelectorAll(".answer").forEach((el) => {
       if (el !== answer) {
         el.style.display = "none";
       }
     });
-
     if (answer.style.display === "block") {
       answer.style.display = "none";
     } else {
